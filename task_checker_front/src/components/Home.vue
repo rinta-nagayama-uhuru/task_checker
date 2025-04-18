78e3a04858fc21974cdeb6054542253fd25eb4e3
P675\P675-2593 <nagayama.rinta@uhuru.jp>
2025-04-18 17:46:46 +0900

    大枠のレイアウトを実装

--- task_checker_front/src/components/Home.vue
+++ task_checker_front/src/components/Home.vue
@@ -2,8 +2,14 @@
 </script>
 
 <template>
-  <h2>Hello world</h2>
+  <div class="main">
+  </div>
 </template>
 
 <style scoped>
+.main {
+  width: 100vw;
+  height: 100vh;
+  background-color: #f6F8F9;
+}
 </style>
