aa9ee08c0e1517bc423344c416b600bddb459c68
P675\P675-2593 <nagayama.rinta@uhuru.jp>
2025-04-24 18:06:59 +0900

    検索結果ページの作成

---
+++ task_checker_front/src/components/SearchResult.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from './Header.vue'
+</script>
+
+<template>
+  <div class="main">
+    <Header />
+    <div class="contents">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
